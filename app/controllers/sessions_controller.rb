405a2f868b744bf3746cd04ffecdac1318b577ad
Elaine Anderson <eparie@bu.edu>
2018-08-15 16:43:16 +0000

    automatically backed up by learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,11 +2,13 @@ class SessionsController < ApplicationController
 
   def create
     binding.pry
-    if params[:name] == nil || params[:name].empty?
-    redirect_to '/login'
-    else
-    session[:name] = params[:name]
-    redirect_to '/'
+    if params[:name]
+      if params[:name] == nil || params[:name].empty?
+      redirect_to '/login'
+      else
+        session[:name] = params[:name]
+        redirect_to '/'
+      end
     end
   end
 
