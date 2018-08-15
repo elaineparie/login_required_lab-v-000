e7e0a60075c909c26c5bb86a1e6caf9ff4957e3a
Elaine Anderson <eparie@bu.edu>
2018-08-15 16:53:58 +0000

    automatically backed up by learn

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   get '/login' => 'sessions#new'
   post '/login' => 'sessions#create'
   post '/logout' => 'sessions#destroy'
-  get '/' => 'secrets#show'
+  #get '/' => 'secrets#show'
 
 
   # The priority is based upon order of creation: first created -> highest priority.
