14c596eced0e3a4f91c389b8506ebc15813f94c7
Elaine Anderson <eparie@bu.edu>
2018-08-15 16:44:53 +0000

    automatically backed up by learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,14 +2,12 @@ class SessionsController < ApplicationController
 
   def create
     binding.pry
-    if params[:name]
       if params[:name] == nil || params[:name].empty?
       redirect_to '/login'
       else
         session[:name] = params[:name]
         redirect_to '/'
       end
-    end
   end
 
   def destroy
